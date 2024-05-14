b288afc82e0845904c157d4f36c51493f6b1817b
Esteban Andres Murcia Acuña <estebandres@proton.me>
2024-05-14 10:46:12 -0500

    Trying stuff idk

--- game_renderer.js
+++ game_renderer.js
@@ -1,5 +1,7 @@
 function startGame(){
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    console.log(con)
+    ctx.moveTo(0,0);
+    ctx.lineTo(200,100);
+    ctx.stroke();
 }
